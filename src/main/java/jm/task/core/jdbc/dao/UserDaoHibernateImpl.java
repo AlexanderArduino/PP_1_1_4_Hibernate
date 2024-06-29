f7015d48dd6cf542be0b2490bf743d13c55278a4
Anokhin <a.i.anohin.arduino@gmail.com>
2024-06-29 16:56:56 +0300

    29.06.2024 Preproject 1.1.4 после замечаний

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -2,8 +2,8 @@ package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
+import org.hibernate.*;
+import org.hibernate.resource.transaction.spi.TransactionStatus;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -26,57 +26,144 @@ public class UserDaoHibernateImpl implements UserDao {
                 "                `lastName` VARCHAR(50) NOT NULL,\n" +
                 "                `age` INT NOT NULL,\n" +
                 "                PRIMARY KEY (`id`));";
-        Session session = sessionFactory.getCurrentSession();
-        session.beginTransaction();
-        session.createSQLQuery(query).executeUpdate();
-        session.getTransaction().commit();
+
+        try (Session session = sessionFactory.getCurrentSession()) {
+            Transaction transaction = session.getTransaction();
+            try {
+                transaction.begin();
+                session.createSQLQuery(query).executeUpdate();
+                transaction.commit();
+                System.out.println("Новая таблица user была создана");
+            } catch (TransactionException transactionException) {
+                if (transaction.getStatus() == TransactionStatus.ACTIVE
+                        || transaction.getStatus() == TransactionStatus.MARKED_ROLLBACK) {
+                    transaction.rollback();
+                    System.out.println("Создание новой таблицы user не удалось");
+                }
+                transactionException.printStackTrace();
+            }
+        } catch (HibernateException hibernateException) {
+            hibernateException.printStackTrace();
+            System.out.println("Создание новой таблицы user. Организация сессии завершилась неудачей");
+        }
     }
 
     @Override
     public void dropUsersTable() {
         String query = "DROP TABLE IF EXISTS `user`";
-        Session session = sessionFactory.getCurrentSession();
-        session.beginTransaction();
-        session.createSQLQuery(query).executeUpdate();
-        session.getTransaction().commit();
+        try (Session session = sessionFactory.getCurrentSession()) {
+            Transaction transaction = session.getTransaction();
+            try {
+                transaction.begin();
+                session.createSQLQuery(query).executeUpdate();
+                transaction.commit();
+                System.out.println("Таблица user была удалена");
+            } catch (TransactionException transactionException) {
+                if (transaction.getStatus() == TransactionStatus.ACTIVE
+                        || transaction.getStatus() == TransactionStatus.MARKED_ROLLBACK) {
+                    transaction.rollback();
+                    System.out.println("Удаление таблицы user не удалось");
+                }
+                transactionException.printStackTrace();
+            }
+        } catch (HibernateException hibernateException) {
+            hibernateException.printStackTrace();
+            System.out.println("Удаление таблицы user. Организация сессии завершилась неудачей");
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        User user = new User(name, lastName, age);
-        Session session = sessionFactory.getCurrentSession();
-        session.beginTransaction();
-        session.save(user);
-        session.getTransaction().commit();
-        session.close();
+        try (Session session = sessionFactory.getCurrentSession()) {
+            Transaction transaction = session.getTransaction();
+            try {
+                transaction.begin();
+                session.save(new User(name, lastName, age));
+                transaction.commit();
+                System.out.println("Новый User был добавлен в таблицу");
+            } catch (TransactionException transactionException) {
+                if (transaction.getStatus() == TransactionStatus.ACTIVE
+                        || transaction.getStatus() == TransactionStatus.MARKED_ROLLBACK) {
+                    transaction.rollback();
+                    System.out.println("Добавление нового User не удалось");
+                }
+                transactionException.printStackTrace();
+            }
+        } catch (HibernateException hibernateException) {
+            hibernateException.printStackTrace();
+            System.out.println("Добавление нового user. Организация сессии завершилась неудачей");
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-        Session session = sessionFactory.getCurrentSession();
-        session.beginTransaction();
-        User user = session.get(User.class, id);
-        session.delete(user);
-        session.getTransaction().commit();
-        session.close();
+        try (Session session = sessionFactory.getCurrentSession()) {
+            Transaction transaction = session.getTransaction();
+            try {
+                transaction.begin();
+                User user = session.get(User.class, id);
+                session.delete(user);
+                transaction.commit();
+                System.out.println("Новый User был удален в таблицу");
+            } catch (TransactionException transactionException) {
+                if (transaction.getStatus() == TransactionStatus.ACTIVE
+                        || transaction.getStatus() == TransactionStatus.MARKED_ROLLBACK) {
+                    transaction.rollback();
+                    System.out.println("Удаление нового User не удалось");
+                }
+                transactionException.printStackTrace();
+            }
+        } catch (HibernateException hibernateException) {
+            hibernateException.printStackTrace();
+            System.out.println("Удаление нового user. Организация сессии завершилась неудачей");
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        Session session = sessionFactory.getCurrentSession();
-        session.beginTransaction();
-        List<User> allUsers = session.createQuery("FROM User").getResultList();
-        session.getTransaction().commit();
-        session.close();
+        List<User> allUsers = new ArrayList<>();
+        try (Session session = sessionFactory.getCurrentSession()) {
+            Transaction transaction = session.getTransaction();
+            try {
+                transaction.begin();
+                allUsers = session.createQuery("FROM User").getResultList();
+                transaction.commit();
+                System.out.println("Новый User был удален в таблицу");
+            } catch (TransactionException transactionException) {
+                if (transaction.getStatus() == TransactionStatus.ACTIVE
+                        || transaction.getStatus() == TransactionStatus.MARKED_ROLLBACK) {
+                    transaction.rollback();
+                    System.out.println("Удаление нового User не удалось");
+                }
+                transactionException.printStackTrace();
+            }
+        } catch (HibernateException hibernateException) {
+            hibernateException.printStackTrace();
+            System.out.println("Удаление нового user. Организация сессии завершилась неудачей");
+        }
         return allUsers;
     }
 
     @Override
     public void cleanUsersTable() {
-        Session session = sessionFactory.getCurrentSession();
-        session.beginTransaction();
-        session.createQuery("delete from User").executeUpdate();
-        session.getTransaction().commit();
-        session.close();
+        try (Session session = sessionFactory.getCurrentSession()) {
+            Transaction transaction = session.getTransaction();
+            try {
+                transaction.begin();
+                session.createQuery("delete from User").executeUpdate();
+                transaction.commit();
+                System.out.println("Таблица User была очищена");
+            } catch (TransactionException transactionException) {
+                if (transaction.getStatus() == TransactionStatus.ACTIVE
+                        || transaction.getStatus() == TransactionStatus.MARKED_ROLLBACK) {
+                    transaction.rollback();
+                    System.out.println("Очистка таблицы User не удалась");
+                }
+                transactionException.printStackTrace();
+            }
+        } catch (HibernateException hibernateException) {
+            hibernateException.printStackTrace();
+            System.out.println("Очистка таблицы user. Организация сессии завершилась неудачей");
+        }
     }
 }
